0d898e36b5fd4e85ce7022eb730fa0378a45f8b9
Ikram Akbar <ikramakbar71@gmail.com>
2022-11-04 11:29:49 +0600

    leetcode Two sum

---
+++ Algorithm/sort.js
@@ -0,0 +1,33 @@
+const arr = [
+  {
+    name: "Prague - train station",
+    value: 1041041,
+    city: "Prague",
+  },
+  {
+    name: "Brno - bus station",
+    value: 104411041,
+    city: "Brno",
+  },
+  {
+    name: "Prague - bus station",
+    value: 104411041,
+    city: "Prague",
+  },
+  {
+    name: "Brno - train station",
+    value: 44411041,
+    city: "Brno",
+  },
+];
+
+let sortedArray = arr.sort((x, y) => {
+    return (x.name.localeCompare(y.name))
+});
+console.log(sortedArray);
+/* [
+  { name: "Brno - bus station", value: 104411041, city: "Brno" },
+  { name: "Brno - train station", value: 44411041, city: "Brno" },
+  { name: "Prague - bus station", value: 104411041, city: "Prague" },
+  { name: "Prague - train station", value: 1041041, city: "Prague" },
+]; */

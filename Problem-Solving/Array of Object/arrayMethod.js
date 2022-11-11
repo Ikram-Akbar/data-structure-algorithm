e3159c1b85c4d15df0e608a40956fc12851d43a8
Ikram Akbar <ikramakbar71@gmail.com>
2022-11-11 17:07:15 +0600

    insert a new object by using unshift

---
+++ Problem-Solving/Array of Object/arrayMethod.js
@@ -0,0 +1,25 @@
+// Array.unshift : - to add an object at the first position --
+
+let cars = [
+  {
+    color: "purple",
+    type: "minivan",
+    registration: new Date("2017-01-03"),
+    capacity: 7,
+  },
+  {
+    color: "red",
+    type: "station wagon",
+    registration: new Date("2018-03-03"),
+    capacity: 5,
+  },
+];
+
+let car = {
+  color: "red",
+  type: "carbo",
+  registration: new Date("2016-05-02"),
+  capacity: 2,
+};
+cars.unshift(car);
+console.log(cars);
